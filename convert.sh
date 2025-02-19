cf4c3c24177a1537050fd10af98bb77065395900
Tavish <tavish9.chen@gmail.com>
2025-02-19 14:35:05 +0800

    add script for conversion
    Co-authored-by: Tavish9 <tavish9.chen@gmail.com>
    Co-authored-by: HaomingSong <1847575517@qq.com>
    Co-authored-by: DelinQu <dlqu22@m.fudan.edu.cn>

---
+++ convert.sh
@@ -0,0 +1,8 @@
+python openx_rlds.py \
+    --raw-dir /path/to/droid/1.0.0 \
+    --local-dir /path/to/LEROBOT_DATASET \
+    --repo-id your_hf_id \
+    --push-to-hub \
+    --batch-size 16 \
+    --num-workers 8 \
+    --use-videos
